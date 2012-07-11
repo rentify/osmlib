0d7aa8e3d267d351de0d143f9b00a531cf133e14
Vitor George <vitor.george@gmail.com>
2012-07-11 15:46:01 -0500

    Updated docs

--- lib/osmlib.rb
+++ lib/osmlib.rb
@@ -1,23 +1,8 @@
 
-# OSMLib is a library for handling OpenStreetMap data and interacting with its API
-# 
-# === Standard Modules
-# 
-# These are the standard module provide by the "osmlib" gem.
-# 
-# * OSMLib::Element is the base module for Node, Way, Relation, Changeset and Tag.
-# * OSMLib::API contains classes for interacting with OpenStreetMap API. 
-# * OSMLib::Database contains classes for interacting with OpenStreetMap API. 
-# * OSMLib::Error contains classes for interacting with OpenStreetMap API. 
-# * OSMLib::OSMChange contains classes for interacting with OpenStreetMap API. 
-# * OSMLib::Stream contains classes for interacting with OpenStreetMap API. 
-
 module OSMLib
 end
 
-
 # Modules for this gem
-
 require 'osmlib/version'
 require 'osmlib/api'
 require 'osmlib/database'
